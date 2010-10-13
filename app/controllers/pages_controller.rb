8a5e8abb64bd7f4f71ef45ec44eae0f4e01eebb9
Brian R Zaik <brian.r.zaik@gmail.com>
2010-10-12 22:04:13 -0600

    Stripped out crap from pages views. I use the word crap a lot.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,6 +15,7 @@ class PagesController < ApplicationController
   # GET /pages/1.xml
   def show
     @page = Page.find(params[:id])
+    @columns = @page.columns
 
     respond_to do |format|
       format.html # show.html.erb
