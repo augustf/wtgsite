55b3d99ff6e98ce4fe9e66389e627e8fdbf89802
August <august@alum.rpi.edu>
2010-10-13 20:16:16 -0400

    Added column views and fixed some forms...

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -16,7 +16,7 @@ class ColumnsController < ApplicationController
     @column = Column.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html {render :layout => false} # show.html.erb (without any layout)
       format.xml  { render :xml => @column }
     end
   end
