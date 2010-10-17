5ce06ceb05bd81150165ad2c42b904c75295ea97
August <august@alum.rpi.edu>
2010-10-16 21:51:25 -0400

    Updated categories scaffolding...

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,35 +1,25 @@
 class CategoriesController < ApplicationController
+  respond_to :html, :xml, :json
+  
   # GET /categories
   # GET /categories.xml
   def index
     @categories = Category.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @categories }
-    end
+    respond_with(@categories)
   end
 
   # GET /categories/1
   # GET /categories/1.xml
   def show
     @category = Category.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @category }
-    end
+    respond_with(@category)
   end
 
   # GET /categories/new
   # GET /categories/new.xml
   def new
     @category = Category.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @category }
-    end
+    respond_with(@category)
   end
 
   # GET /categories/1/edit
@@ -41,43 +31,21 @@ class CategoriesController < ApplicationController
   # POST /categories.xml
   def create
     @category = Category.new(params[:category])
-
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to(@category, :notice => 'Category was successfully created.') }
-        format.xml  { render :xml => @category, :status => :created, :location => @category }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @category.errors, :status => :unprocessable_entity }
-      end
-    end
+    flash[:notice] = "Category successfully created" if @category.save
+    respond_with(@category)
   end
 
   # PUT /categories/1
   # PUT /categories/1.xml
   def update
     @category = Category.find(params[:id])
-
-    respond_to do |format|
-      if @category.update_attributes(params[:category])
-        format.html { redirect_to(@category, :notice => 'Category was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @category.errors, :status => :unprocessable_entity }
-      end
-    end
+    @category(params[:category])
+    respond_with(@category)
   end
 
   # DELETE /categories/1
   # DELETE /categories/1.xml
   def destroy
-    @category = Category.find(params[:id])
-    @category.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(categories_url) }
-      format.xml  { head :ok }
-    end
-  end
+    @category = Category.find(params[:id]).destroy
+    respond_with(@category)
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,5 @@
 class Page < ActiveRecord::Base
+  belongs_to :category
   has_many :columns
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
