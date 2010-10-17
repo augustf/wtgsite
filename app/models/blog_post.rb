726a2da3a965767414f5bb9eb7ec628630428c01
Brian R Zaik <brian.r.zaik@gmail.com>
2010-10-17 15:44:43 -0600

    Added some more plumbing work to blog pages

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -3,6 +3,7 @@ class BlogPost < ActiveRecord::Base
   has_friendly_id :title, :use_slug => true
   belongs_to :blog_category
 	has_many :blog_comments
+	belongs_to :user
 	validates :title, :presence => true
 	validates :body, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
 	acts_as_authentic
 	has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_many :blog_posts
 end
